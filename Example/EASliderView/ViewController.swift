f0d3393f221faff391f70cf1970028fc6ba83742
Emre AYDIN <ea@ea.tc>
2020-01-14 11:50:32 +0300

    EASliderView customization..

--- Example/EASliderView/ViewController.swift
+++ Example/EASliderView/ViewController.swift
@@ -16,13 +16,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Seting the values
+        sliderView.title = "Title here"
+        sliderView.columns = ["Option 1", "Option 2", "Option 3"]
+        
+        // Customizing slider
         sliderView.dotActiveColor = .brown
         sliderView.dotPassiveColor = .red
         sliderView.lineActiveColor = .red
         sliderView.linePassiveColor = .black
         sliderView.thumbColor = .brown
-        sliderView.title = "Title here"
-        sliderView.columns = ["Option 1", "Option 2", "Option 3"]
+        
+        sliderView.titleLabelFont = UIFont.systemFont(ofSize: 14)
+        sliderView.titleLabelColor = .black
+        sliderView.columnLabelFont = UIFont.systemFont(ofSize: 13)
+        sliderView.columnLabelColor = .gray
     }
 
     override func didReceiveMemoryWarning() {
--- Example/Pods/Target Support Files/Pods-EASliderView_Example/Pods-EASliderView_Example-acknowledgements.markdown
+++ Example/Pods/Target Support Files/Pods-EASliderView_Example/Pods-EASliderView_Example-acknowledgements.markdown
@@ -3,7 +3,7 @@ This application makes use of the following third party libraries:
 
 ## EASliderView
 
-Copyright (c) 2019 aydin@emre <ea@ea.tc>
+Copyright (c) 2019 aydin-emre <ea@ea.tc>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
